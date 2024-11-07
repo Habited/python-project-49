2dcb2e2511a84bd30033e748713f48dee965471e
Habited <pdskate88@gmail.com>
2024-11-07 22:19:18 +0300

    added setup.cfg, brain_games/cli.py files; updated brain_games/scripts/brain_games.py, makefile, pyproject.toml files

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcom_user
+
+
 def main():
-    print("poetry run python -m brain_games.scripts.brain_games")
     print("Welcome to the Brain Games!")
+    welcom_user()
+
 
 if __name__ == '__main__':
     main()
