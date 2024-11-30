7b53c891f84023e129d91f07cbcee3739d919b15
Habited <pdskate88@gmail.com>
2024-11-30 18:03:50 +0300

    update: file brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,19 +2,19 @@ import prompt
 
 
 def run_games(instruction, number_of_rounds,
-              get_answer_and_player_responce):
+              get_answer_and_question):
     name: str = prompt.string('Welcome to the Brain Games!\n'
                               'May I have your name? ')
     print(f'Hello, {name}!\n'
           f'{instruction}')
 
     for _ in range(number_of_rounds):
-        correct_answer, question = get_answer_and_player_responce()
+        correct_answer, question = get_answer_and_question()
         player_responce: str = input(f'Question: {question}\nYour answer: ')
         if correct_answer == player_responce:
             print('Correct!')
         else:
-            print(f"'{question}' is wrong answer ;(."
+            print(f"'{player_responce}' is wrong answer ;(."
                   f"'Correct answer was '{correct_answer}'.\n"
                   f"Let's try again, {name}!")
             return
