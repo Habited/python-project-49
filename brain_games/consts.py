c2e060e9c53edfbdebbb5ea2dc5d465c1bdb9206
Habited <pdskate88@gmail.com>
2024-11-30 17:44:08 +0300

    update: files: brain_games/consts.py, brain_games/scripts/brain_progression.py,pyproject.toml

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -1,5 +1,6 @@
 EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC = "What is the result of the expression?"
 GCD = "Find the greatest common divisor of given numbers."
+PROGRESSION = "What number is missing in the progression?"
 MATHEMATICAL_SYMBOL: list[str, str] = ['+', '-', '*']
 NUMBER_OF_ROUNDS = 3
