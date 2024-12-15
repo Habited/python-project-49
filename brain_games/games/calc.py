a6fb5a5fdf4547b45689f0903f10a82cc25ba175
Habited <pdskate88@gmail.com>
2024-12-15 22:00:52 +0300

    fix: return str(result), question

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -14,7 +14,7 @@ def get_math_question_and_result():
     first_num, second_num = randint(1, 100), randint(1, 100)
     sign, result = get_random_math_sign_and_result(first_num, second_num)
     question = f"{first_num} {sign} {second_num}"
-    return question, str(result)
+    return str(result), question
 
 
 def run_calc_games():
